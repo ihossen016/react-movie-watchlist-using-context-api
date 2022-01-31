8b2ef3da0eb8598c691274c520892c421b52d18b
ihossen016 <ihossen016@gmail.com>
2022-01-31 14:26:30 +0600

    react movie watchlist project completed

---
+++ src/context/GlobalState.js
@@ -0,0 +1,64 @@
+import React, { createContext, useReducer, useEffect } from "react";
+import AppReducer from "./AppReducer";
+
+// Initial Sate
+const initialState = {
+  watchlist: localStorage.getItem("watchlist")
+    ? JSON.parse(localStorage.getItem("watchlist"))
+    : [],
+  watched: localStorage.getItem("watched")
+    ? JSON.parse(localStorage.getItem("watched"))
+    : [],
+};
+
+// Create Context
+export const GlobalContext = createContext(initialState);
+
+// provider components
+export const GlobalProvider = props => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  useEffect(() => {
+    localStorage.setItem("watchlist", JSON.stringify(state.watchlist));
+    localStorage.setItem("watched", JSON.stringify(state.watched));
+  }, [state]);
+
+  // Actions
+  const addMovieToWatchlist = movie => {
+    dispatch({ type: "ADD_MOVIE_TO_WATCHLIST", payload: movie });
+  };
+
+  const removeMovieFromWatchlist = id => {
+    dispatch({ type: "REMOVE_MOVIE_FROM_WATCHLIST", payload: id });
+  };
+
+  const addMovieToWatched = movie => {
+    dispatch({ type: "ADD_MOVIE_TO_WATCHED", payload: movie });
+  };
+
+  // Move to Watch List
+  const moveToWatchlist = movie => {
+    dispatch({ type: "MOVE_TO_WATCHLIST", payload: movie });
+  };
+
+  // Remove from Watched List
+  const removeFromWatched = id => {
+    dispatch({ type: "REMOVE_FROM_WATCHED", payload: id });
+  };
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        watchlist: state.watchlist,
+        watched: state.watched,
+        addMovieToWatchlist,
+        removeMovieFromWatchlist,
+        addMovieToWatched,
+        moveToWatchlist,
+        removeFromWatched,
+      }}
+    >
+      {props.children}
+    </GlobalContext.Provider>
+  );
+};
